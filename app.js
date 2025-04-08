648ce6c0d9475a23d25f88b8ff6c432f949098a3
Joel4545Coelho <joelcoelho1309@gmail.com>
2025-04-08 21:37:54 +0000

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(
 );
 
 const PORT = process.env.PORT || 25565;
-const DATABASE_URL = "mongodb://joelcoelho1309:12345@ac-vb4qym0-shard-00-00.1kdd3py.mongodb.net:27017,ac-vb4qym0-shard-00-01.1kdd3py.mongodb.net:27017,ac-vb4qym0-shard-00-02.1kdd3py.mongodb.net:27017/autocare?replicaSet=atlas-qsytdp-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=Cluster0";
+const DATABASE_URL = "mongodb+srv://joelcoelho1309:12345@cluster0.1kdd3py.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 mongoose
   .connect(DATABASE_URL, {
