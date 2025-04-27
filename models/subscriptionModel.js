fb76e8fce5a400df81774ec2ca1145824ec2af1e
Theinfected137 <gabifortnite2008@gmail.com>
2025-04-27 22:43:20 +0000

    haha

--- models/subscriptionModel.js
+++ models/subscriptionModel.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const SubscriptionSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
   planoId: { type: mongoose.Schema.Types.ObjectId, ref: 'Plano' },
-  dataInicio: Date,
-  dataFim: Date,
+  dataInicio: { type: Date, default: Date.now },
+  dataFim: { type: Date, required: true },
   status: { type: String, enum: ['active', 'canceled', 'expired'], default: 'active' },
   paymentStatus: { type: String, enum: ['pending', 'completed', 'failed'], default: 'pending' },
   level: { type: String, enum: ["free", "basic", "premium", "medico"], required: true }
