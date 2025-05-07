21bca23766258506f43dbe8a3f7cf958620e6cf9
Joel4545Coelho <joelcoelho1309@gmail.com>
2025-05-07 13:30:35 +0000

    Update receita_medico.js

--- controllers/receita_medico.js
+++ controllers/receita_medico.js
@@ -17,34 +17,35 @@ const getReceitas = async (req, res) => {
   }
 };
 
-const addReceita = async (req, res) => {
-  try {
-    const { description, medicoId, pacienteId } = req.body;
+  const addReceita = async (req, res) => {
+    try {
+      const { description, medicoId, pacienteId } = req.body;
 
-    if (!req.file) {
-      return res.status(400).json({ error: "Nenhum arquivo foi enviado." });
+      if (!req.file) {
+        return res.status(400).json({ error: "Nenhum arquivo foi enviado." });
+      }
+      console.log("Uploaded file info:", req.file);
+
+      const novaReceita = new receitaMedico({
+        description,
+        medicoId,
+        pacienteId,
+        fileUrl: req.file.location, // S3 public URL
+        fileName: req.file.key,
+        fileType: req.file.mimetype,
+        fileSize: req.file.size
+      });
+
+      await novaReceita.save();
+
+      res.status(201).json({
+        success: true,
+        data: novaReceita
+      });
+    } catch (error) {
+      console.error("Erro ao adicionar receita:", error);
+      res.status(500).json({ error: "Erro ao adicionar receita." });
     }
-
-    const novaReceita = new receitaMedico({
-      description,
-      medicoId,
-      pacienteId,
-      fileUrl: req.file.location, // S3 public URL
-      fileName: req.file.key,
-      fileType: req.file.mimetype,
-      fileSize: req.file.size
-    });
-
-    await novaReceita.save();
-
-    res.status(201).json({
-      success: true,
-      data: novaReceita
-    });
-  } catch (error) {
-    console.error("Erro ao adicionar receita:", error);
-    res.status(500).json({ error: "Erro ao adicionar receita." });
-  }
-};
+  };
 
 module.exports = { getReceitas, addReceita };
