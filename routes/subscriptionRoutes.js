bb3271f5e85762e57a807d54f87d1b6c258995ce
Theinfected137 <gabifortnite2008@gmail.com>
2025-05-08 16:19:31 +0000

    maybe

--- routes/subscriptionRoutes.js
+++ routes/subscriptionRoutes.js
@@ -1,17 +1,17 @@
-// routes/subscriptionRoutes.js
 const express = require('express');
 const router = express.Router();
 const subscriptionController = require('../controllers/subscriptionController');
-const easyController = require('../controllers/easyController');
-const { auth } = require('../middlewares/IsAuth');
+const easypayController = require('../controllers/easypayController');
+const auth = require("../middlewares/IsAuth");
 
 router.get('/planos', auth, subscriptionController.getPlanos);
 router.get('/meu-plano', auth, subscriptionController.getPlanoAtual);
 router.post('/assinar', auth, subscriptionController.assinarPlano);
 router.post('/cancelar', auth, subscriptionController.cancelarPlano);
+router.post('/pagamento', auth, subscriptionController.initiatePayment);
 
 // EasyPay routes
-router.post('/assinar-easypay', auth, easyController.createEasyPaySubscription);
-router.get('/check-status/:subscriptionId', auth, easyController.checkSubscriptionStatus);
+router.post('/assinar-easypay', auth, easypayController.createEasyPaySubscription);
+router.get('/check-status/:subscriptionId', auth, easypayController.checkSubscriptionStatus);
 
 module.exports = router;
