cf1b39414255d54691d42a804b79ababd2f6a064
Theinfected137 <gabifortnite2008@gmail.com>
2025-05-08 15:47:16 +0000

    meti o nome errado :/

--- routes/subscriptionRoutes.js
+++ routes/subscriptionRoutes.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const subscriptionController = require('../controllers/subscriptionController');
-const easypayController = require('../controllers/easypayController');
+const easyController = require('../controllers/easyController');
 const { auth } = require('../middleware/authMiddleware');
 
 router.get('/planos', auth, subscriptionController.getPlanos);
@@ -11,7 +11,7 @@ router.post('/assinar', auth, subscriptionController.assinarPlano);
 router.post('/cancelar', auth, subscriptionController.cancelarPlano);
 
 // EasyPay routes
-router.post('/assinar-easypay', auth, easypayController.createEasyPaySubscription);
-router.get('/check-status/:subscriptionId', auth, easypayController.checkSubscriptionStatus);
+router.post('/assinar-easypay', auth, easyController.createEasyPaySubscription);
+router.get('/check-status/:subscriptionId', auth, easyController.checkSubscriptionStatus);
 
 module.exports = router;
