199f42d9c070cc35f49214ae9b87b56f17715e4b
Joel4545Coelho <joelcoelho1309@gmail.com>
2025-05-08 16:02:58 +0000

    aaa

--- routes/profileroutes.js
+++ routes/profileroutes.js
@@ -5,16 +5,10 @@ const multer = require('multer');
 const path = require('path');
 const authenticate = require('../middlewares/IsAuth'); // Middleware de autenticação
 
-// Configuração do Multer para o upload de arquivos
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, 'uploads/'),
-  filename: (req, file, cb) => cb(null, Date.now() + path.extname(file.originalname))
-});
-
 const upload = multer({ storage });
 
 // Rota para atualizar o avatar
-router.post('/update-avatar', authenticate, upload.single('avatar'), profileController.updateAvatar);
+router.post('/update-avatar', authenticate, upload.single('file'), profileController.updateAvatar);
 
 // Rota para atualizar o nome e email
 router.put('/update-profile', authenticate, profileController.updateProfile);
