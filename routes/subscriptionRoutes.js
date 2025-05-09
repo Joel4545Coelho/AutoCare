6822112bb3d6f0e07783af683e58663ff94eb2e7
Marcio9736 <marciopamplona8@gmail.com>
2025-05-09 16:57:58 +0000

    fix at atempt6

--- routes/subscriptionRoutes.js
+++ routes/subscriptionRoutes.js
@@ -13,5 +13,6 @@ router.post('/pagamento', auth, subscriptionController.initiatePayment);
 // EasyPay routes
 router.post('/assinar-easypay', auth, easyController.createEasyPaySubscription);
 router.get('/check-status/:subscriptionId', auth, easyController.checkSubscriptionStatus);
+router.post('/cancel-pending/:subscriptionId', auth, easyController.cancelPendingSubscription);
 
 module.exports = router;
