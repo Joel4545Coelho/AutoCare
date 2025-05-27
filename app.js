cb18b5528544bf982db767ed72739661fd4b53e0
Joana <4544@feppv.pt>
2025-05-27 12:03:00 +0100

    no message

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const profileroutes = require('./routes/profileroutes');
 const subroutes = require('./routes/subscriptionRoutes');
 const receitasP = require('./routes/receitasPaciente');
 const receitas_medico = require('./routes/receitas_medico')
-const firebaseRoutes = require('/routes/firebaseRoutes');
+const firebaseRoutes = require('./routes/firebaseRoutes');
 const RatingRoutes = require('./routes/ratingRoutes').default;
 
 const DATABASE_URL = "mongodb://joelcoelho1309:12345@ac-vb4qym0-shard-00-00.1kdd3py.mongodb.net:27017,ac-vb4qym0-shard-00-01.1kdd3py.mongodb.net:27017,ac-vb4qym0-shard-00-02.1kdd3py.mongodb.net:27017/autocare?replicaSet=atlas-qsytdp-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=Cluster0";
